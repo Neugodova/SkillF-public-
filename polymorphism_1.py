cdba4fb56b3f18b8bd46e637aa530d3828bb1c67
Kris <96240770+Neugodova@users.noreply.github.com>
2022-11-24 21:06:06 +0500

    Add task 16.8.2(2)
    Added part 2 task 16.8.2

---
+++ polymorphism_1.py
@@ -0,0 +1,35 @@
+from polymorphism import Rectangle, Square, Circle
+
+# создание 2-х прямоугольников
+rect_1 = Rectangle(3, 4)
+rect_2 = Rectangle(5, 6)
+
+# вывод площади 2-х прямоугольников
+print(rect_1.get_area(),
+      rect_2.get_area())
+
+sq_1 = Square(5)
+sq_2 = Square(10)
+
+# вывод возведения в степень **2()
+
+print(sq_1.get_area_square(),
+      sq_2.get_area_square())
+
+circ_1 = Circle(3)
+circ_2 = Circle(5)
+
+print(circ_1.get_area_circle(),
+      circ_2.get_area_circle())
+
+#
+figures = [rect_1, rect_2, sq_1, sq_2, circ_1, circ_2]
+
+# цикл, в котором программа выводит название фигуры
+for figure in figures:
+      if isinstance(figure, Square):
+            print("It's a square:", figure.get_area_square())
+      elif isinstance(figure, Circle):
+            print("It's a circle:", figure.get_area_circle())
+      else:
+            print("It's a rectangle:",figure.get_area())
